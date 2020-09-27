945d66436304ce62ffebadb83f138343088c1db3
sanjay <sanjay.igit14@gmail.com>
2020-09-27 12:35:39 +0530

    Updates

---
+++ precache-manifest.35e291a805402fac104343133aaab791.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76b190cffe56534ad4ce4a3614a77665",
+    "url": "/sirweb/index.html"
+  },
+  {
+    "revision": "18853fe7405246a7fbc8",
+    "url": "/sirweb/static/css/main.6b72a5cf.chunk.css"
+  },
+  {
+    "revision": "6840164ce5086ac6474f",
+    "url": "/sirweb/static/js/2.14c247ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sirweb/static/js/2.14c247ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18853fe7405246a7fbc8",
+    "url": "/sirweb/static/js/main.67827bb5.chunk.js"
+  },
+  {
+    "revision": "38ee0fd72b396422c2a9",
+    "url": "/sirweb/static/js/runtime-main.aa8e32dc.js"
+  }
+]);
